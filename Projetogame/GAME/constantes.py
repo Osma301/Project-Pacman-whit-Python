2e2e452d837f1fe052a6fd12ae1bb20d41982c85
Osmando Alfonso D Jesus M R <106894809+Osma301@users.noreply.github.com>
2022-08-04 20:48:09 -0300

    Add files via upload

---
+++ Projetogame/GAME/constantes.py
@@ -0,0 +1,30 @@
+#Dimensões da tela
+LARGURA = 480
+ALTURA = 680
+
+#TITULO DO JOGO
+
+TITULO_JOGO = "PAC-RALHO"
+
+#FPS
+
+FPS = 30
+
+#Cores
+
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+AMARELO = (244, 233 , 51)
+
+
+#Imagens
+
+SPRITESHEET = 'spritesheet.png'
+PACMAN_START_LOGO = 'pacman-logo-1.png'
+
+#FONTE 
+FONTE = "arial"
+
+#AUDIOS
+MUSICA_START = 'intermission.wav'
+TECLA_START = 'munch_1.wav'
